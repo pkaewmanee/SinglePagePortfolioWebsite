e8fbe5d665c4421100124d5698326a3401930e33
Phakkhapon Kaewmanee <fishermanfill01@gmail.com>
2025-01-20 15:16:40 +0700

    yes

---
+++ script.js
@@ -0,0 +1,31 @@
+// script.js
+
+function addRecommendation() {
+    // Get the message of the new recommendation
+    let recommendation = document.getElementById("new_recommendation");
+  
+    // If the user has left a recommendation, display a pop-up
+    if (recommendation.value != null && recommendation.value.trim() !== "") {
+      console.log("New recommendation added");
+  
+      // Call showPopup to confirm submission
+      showPopup(true);
+  
+      // Create a new 'recommendation' element and set its value to the user's message
+      let element = document.createElement("div");
+      element.setAttribute("class", "recommendation");
+      element.innerHTML = `<span>&#8220;</span>${recommendation.value}<span>&#8221;</span>`;
+  
+      // Add this elem;sent to the end of the list of recommendations
+      document.getElementById("all_recommendations").appendChild(element);
+  
+      // Reset the value of the textarea
+      recommendation.value = "";
+    }
+  }
+  
+  function showPopup(isVisible) {
+    const popup = document.getElementById('popup');
+    popup.style.visibility = isVisible ? 'visible' : 'hidden';
+  }
+  
